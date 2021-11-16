ebfb56caad0ffd231cd788accf2692e9985cd00b
Theo Molteno <theo@molteno.net>
2021-11-16 19:04:42 +1300

    fixed import of circular buffer header file

--- test_buf.cpp
+++ test_buf.cpp
@@ -1,9 +1,9 @@
 /** Creates an instance of a circular buffer for the
  * purposes of testing it
  */
-#include <circular_buffer.hpp>
+#include "circular_buffer.hpp"
 
 int main(void) {
-    circular_buffer cb();
+    circular_buffer cb = circular_buffer();
     cb.dump();
 }
