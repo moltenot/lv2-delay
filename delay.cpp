981a61e07e141da0c671064313329afd6a645450
Theo Molteno <theo@molteno.net>
2021-11-17 20:23:22 +1300

    added delay of fixed length

--- delay.cpp
+++ delay.cpp
@@ -1,7 +1,9 @@
 #include "dsp-utils/functions.hpp"
+#include "circular_buffer.hpp"
+#include "constants.hpp"
+
 #include <lv2plugin.hpp>
 #include <iostream>
-#include <constants.hpp>
 
 
 static const float max_delay_seconds = 2.0;
@@ -10,11 +12,13 @@ class delay : public LV2::Plugin<delay>
 {
 private:
     double sample_rate;
+    circular_buffer cb;
 
 public:
     delay(double sample_rate) : LV2::Plugin<delay>(3)
     { // since there are 3 ports
         this->sample_rate = sample_rate;
+        this->cb = circular_buffer(); // consulte the constants file for how long this is
         std::cout << "\nsample rate is: " << sample_rate << std::endl;
     };
 
@@ -23,7 +27,23 @@ public:
         std::cout << "called run" << std::endl;
         for (size_t i = 0; i < sample_count; i++)
         {
-            p(OUTPUT_PORT_INDEX)[i] = p(INPUT_PORT_INDEX)[i];
+            float echo;
+            float &delay = *p(DELAY_PORT_INDEX); // the delay we want in seconds
+            
+            // set the echo to 0 if we need to wait longer, or pop off the circular
+            // buffer if we are ready to go
+            if (this->cb.get_delta() >= delay*this->sample_rate) 
+            {
+                echo = this->cb.pop();
+            } else {
+                echo = 0.0;
+            }
+            std::cout << "echo = " << echo << std::endl;
+
+            // append the input to the circular buffer
+            this->cb.put(p(INPUT_PORT_INDEX)[i]); 
+
+            p(OUTPUT_PORT_INDEX)[i] = p(INPUT_PORT_INDEX)[i] + 0.5*echo;
         }
     }
 };
