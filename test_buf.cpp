fa465efae5dc1cfab70141fe73e15988c3fc1c61
Theo Molteno <theo@molteno.net>
2021-11-17 19:29:11 +1300

    added tests

--- test_buf.cpp
+++ test_buf.cpp
@@ -2,8 +2,30 @@
  * purposes of testing it
  */
 #include "circular_buffer.hpp"
+#include <iostream>
 
 int main(void) {
     circular_buffer cb = circular_buffer();
     cb.dump();
+    cb.put(2.1);
+    cb.dump();
+    cb.put(3.2);
+    cb.dump();
+    cb.put(4.3);
+    cb.dump();
+    cb.put(5.5);
+    cb.dump();
+    cb.put(4.7);
+    cb.dump();
+    cb.put(8.7);
+    cb.dump();
+
+    std::cout << "cb.pop() = " << cb.pop() << std::endl;
+    std::cout << "cb.pop() = " << cb.pop() << std::endl;
+    std::cout << "cb.pop() = " << cb.pop() << std::endl;
+    std::cout << "cb.pop() = " << cb.pop() << std::endl;
+    std::cout << "cb.pop() = " << cb.pop() << std::endl;
+    std::cout << "cb.pop() = " << cb.pop() << std::endl;
+    
+    
 }
